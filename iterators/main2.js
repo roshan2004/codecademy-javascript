33ef9a7be3fba19dde6f7ddd6bba19ef995e2ab8
Roshan Shrestha <roshan_pra@hotmail.com>
2020-06-18 14:54:49 +0545

    Added foreach code

---
+++ iterators/main2.js
@@ -0,0 +1,9 @@
+/*.forEach() will execute the same code for each element of an array.
+*/
+const groceries = ['brown sugar', 'salt', 'cranberries', 'walnuts'];
+
+groceries.forEach(item => console.log(item));
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(item => console.log('I want to eat a '+item));
